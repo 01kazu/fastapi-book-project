a75cebfc8498e57b22245e338676f8776961fb24
Joshua Yakubu David <30726966+01kazu@users.noreply.github.com>
2025-02-17 12:40:50 +0100

    chore: update integration_conf.py

--- api/routes/integration_conf.py
+++ api/routes/integration_conf.py
@@ -19,7 +19,7 @@ integration_json = {
       "background_color": "#fff"
     },
     "is_active": True,
-    "integration_type": "modifier"
+    "integration_type": "modifier",
     "key_features": [
       "realtime-updates",
       "click notifications"
