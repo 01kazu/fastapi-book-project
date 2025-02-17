9a8bb811bd95636775d33dcfd4d970aa7d9769d2
Joshua Yakubu David <30726966+01kazu@users.noreply.github.com>
2025-02-17 18:03:33 +0100

    chore: add webhook router

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from api.router import api_router
-from api.routes.integration_conf import router as integration_route
+from api.routes.integration_conf import router as integration_router
+from api.routes.webhook import router as webhook_router
 from core.config import settings
 
 app = FastAPI()
@@ -16,7 +17,8 @@ app.add_middleware(
 )
 
 # Integration router
-app.include_router(integration_route)
+app.include_router(integration_router)
+app.include_router(webhook_router)
 app.include_router(api_router, prefix=settings.API_PREFIX)
 
 
